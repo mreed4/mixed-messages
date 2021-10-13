4ec3d9237381830682b426f79c2f2720ed94936a
Matthew <78713317+mreed4@users.noreply.github.com>
2021-10-12 22:29:20 -0700

    Got random year working

--- main.js
+++ main.js
@@ -1,1 +1,35 @@
-console.log(`Hello world`)
+// console.log(`Hello world`);
+
+const randYear = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+
+    let year = Math.floor(Math.random() * (max - min + 1) + min);
+
+    return year;
+};
+
+const yearSet = (set) => {
+    let ancientYear = -3000;
+    let currentYear = new Date().getFullYear();
+
+    let years = [];
+    let n = 1;
+
+    while (n <= set) {
+        years.push(randYear(ancientYear, currentYear));
+        n++;
+    }
+
+    let exiguus = years.map((n) => {
+        if (n < 0) {
+            return `${-n} B.C.`;
+        } else {
+            return `A.D. ${n}`;
+        }
+    });
+
+    console.log(exiguus);
+};
+
+yearSet(30);
