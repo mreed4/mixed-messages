18f026b9bf7f6a48f3b6c46168fd5ea8542dc10d
Matthew <78713317+mreed4@users.noreply.github.com>
2021-10-13 08:29:57 -0700

    More functions (pull random year from generated set)

--- main.js
+++ main.js
@@ -10,26 +10,41 @@ const randYear = (min, max) => {
 };
 
 const yearSet = (set) => {
-    let ancientYear = -3000;
+    let antiquityYear = -3000;
     let currentYear = new Date().getFullYear();
 
     let years = [];
     let n = 1;
-
     while (n <= set) {
-        years.push(randYear(ancientYear, currentYear));
+        years.push(randYear(antiquityYear, currentYear));
         n++;
     }
 
-    let exiguus = years.map((n) => {
-        if (n < 0) {
-            return `${-n} B.C.`;
+    let exiguus = years.map((year) => {
+        if (year < 0) {
+            return `${-year} B.C.`;
         } else {
-            return `A.D. ${n}`;
+            return `A.D. ${year}`;
         }
     });
 
-    console.log(exiguus);
+    // console.log(exiguus);
+    return exiguus;
 };
 
-yearSet(30);
+const quoteYear = (years) => {
+    let i = Math.round(Math.random() * years.length);
+
+    return years[i];
+};
+
+let set30 = yearSet(30);
+
+let year = quoteYear(set30);
+
+console.log(year);
+
+const quoteSet = (set) => {};
+
+const generateFakeQuote = () => {};
+// yearSet(set);
